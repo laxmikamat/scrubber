b721dca6bb9c4bc5aa4429feebe0ee9d38ee1bda
Ashwin Singh <32096670+asterisx2@users.noreply.github.com>
2018-10-19 15:33:19 +0530

    Feat key creator page

--- utils.js
+++
@@ -1,6 +0,0 @@
-function convertToKey(node) {
-    let attrArray = Array.prototype.slice.call(node.attributes);
-    let valueToReturn = node.nodeName.toLowerCase().toString();
-    valueToReturn += attrArray.reduce((acc, curr) => acc += ("::" + curr.name + "='" + curr.value + "'"), '');
-    return valueToReturn;
-}
